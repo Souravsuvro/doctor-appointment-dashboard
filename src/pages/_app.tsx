51e1816e85848c34e4466c200a345d61c3dd2275
prabal23 <rockingrider.suvro@gmail.com>
2025-02-20 03:53:20 +0000

    Update globals.css import path to correct location

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import '../app/globals.css';
 
 config.autoAddCss = false;
 
