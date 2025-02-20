aa74afa11e12870f7fd3225b39843fb1aea6e61c
prabal23 <rockingrider.suvro@gmail.com>
2025-02-20 04:00:25 +0000

    Explicitly import Tailwind CSS in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import type { AppProps } from 'next/app';
+import 'tailwindcss/tailwind.css';
 import '../app/globals.css';
 
 config.autoAddCss = false;
